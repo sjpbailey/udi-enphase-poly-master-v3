b10790f25dc40ae7cfe0ba52cd7171c2334663fe
sjpbailey <sjpbailey1961@gmail.com>
2021-11-23 01:31:22 -0800

    Update ENPHASE.py

--- ENPHASE.py
+++ ENPHASE.py
@@ -17,8 +17,7 @@ if __name__ == "__main__":
         polyglot.start()
 
         # Create the controller node
-        EnphaseController.Controller(
-            polyglot, 'controller', 'controller', 'Enphase')
+        EnphaseController(polyglot, 'controller', 'controller', 'Enphase')
 
         # Just sit and wait for events
         polyglot.runForever()
