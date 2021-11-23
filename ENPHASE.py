0a0192d660a13a3535efefff407ec10f5b912c2b
sjpbailey <sjpbailey1961@gmail.com>
2021-11-23 01:47:34 -0800

    Commits

--- ENPHASE.py
+++ ENPHASE.py
@@ -1,27 +1,28 @@
-#!/usr/bin/env python
-from nodes import EnphaseController
+#!/usr/bin/env python3
+"""
+Polyglot v3 node server Example 2
+Copyright (C) 2021 Robert Paauwe
+
+MIT License
+"""
 import udi_interface
 import sys
 
-LOGGER = udi_interface.LOGGER
+from nodes import irrigation_ctl
 
-""" Grab My Controller Node (optional) """
+LOGGER = udi_interface.LOGGER
 
 if __name__ == "__main__":
     try:
-
-        polyglot = udi_interface.Interface([EnphaseController])
-
+        polyglot = udi_interface.Interface([])
         polyglot.start()
 
-        control = EnphaseController(
-            polyglot, 'controller', 'controller', 'PythonTemplate')
+        # Create the controller node
+        irrigation_ctl.Controller(polyglot, 'controller', 'controller', 'Irrigation Control')
 
+        # Just sit and wait for events
         polyglot.runForever()
     except (KeyboardInterrupt, SystemExit):
-        LOGGER.warning("Received interrupt or exit...")
+        sys.exit(0)
+        
 
-        polyglot.stop()
-    except Exception as err:
-        LOGGER.error('Excption: {0}'.format(err), exc_info=True)
-    sys.exit(0)
