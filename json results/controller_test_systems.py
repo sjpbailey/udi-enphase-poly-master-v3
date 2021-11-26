f0da3479d0ba9f4da8309745e1291ac756643c5d
sjpbailey <sjpbailey1961@gmail.com>
2021-11-25 22:59:35 -0800

    Commits

--- json results/controller_test_systems.py
+++ json results/controller_test_systems.py
@@ -7,14 +7,15 @@ data = f.read()
 systemResponse = json.loads(data)
 
 #print('\n System ID \n', systemResponse["systems"][0]["system_id"])
+#print('\n System ID \n', systemResponse["systems"][0:2])
 #print('\n System Status \n', systemResponse["systems"][0]["status"])
 #print('\n System Country \n', systemResponse["systems"][0]["country"])
 #print('\n System ID \n', systemResponse["systems"][1]["system_id"])
 #print('\n System Status \n', systemResponse["systems"][1]["status"])
 #print('\n System Country \n', systemResponse["systems"][1]["country"])
-hellohere = systemResponse["systems"][0:1]
+hellohere = systemResponse["systems"][0:2]
 # print(hellohere)
 #num_sites = (print('\n System One \n', systemResponse["systems"][0]))
 #num_sites = (print('\n System Two \n', systemResponse["systems"][1]))
 for i in hellohere:
-    print('\n', i,  hellohere[i])
+    print('\n', i,  hellohere[0:1])
--- json results/test_enphase.py
+++ json results/test_enphase.py
@@ -28,23 +28,23 @@ end_date = presentday.strftime('%Y-%m-%d')
 end_scnd = second.strftime('%Y-%m-%d')
 end_tird = third.strftime('%Y-%m-%d')
 
-print(start_date)
-print(end_date)
-print(end_scnd)
-print(end_tird)
+# print(start_date)
+# print(end_date)
+# print(end_scnd)
+# print(end_tird)
 
 
 # 'https://api.enphaseenergy.com/api/v2/systems/2527105/energy_lifetime?start_date=2021-11-10&end_date=2021-11-25'
 # 'https://api.enphaseenergy.com/api/v2/systems/2527105/energy_lifetime?+start_date+end_date'
 response2 = requests.get(
     'https://api.enphaseenergy.com/api/v2/systems/2527105/energy_lifetime?start_date='+start_date+'&end_date='+end_date,  params=params).text
-print('\n Lifetime Energy Daily Report \n' + response2)
+#print('\n Lifetime Energy Daily Report \n' + response2)
 response3 = requests.get(
     'https://api.enphaseenergy.com/api/v2/systems/2527105/energy_lifetime?start_date='+end_scnd+'&end_date='+end_scnd,  params=params).text
-print('\n Lifetime Energy Daily Report \n' + response3)
+#print('\n Lifetime Energy Daily Report \n' + response3)
 response4 = requests.get(
     'https://api.enphaseenergy.com/api/v2/systems/2527105/energy_lifetime?start_date='+end_tird+'&end_date='+end_tird,  params=params).text
-print('\n Lifetime Energy Daily Report \n' + response4)
+#print('\n Lifetime Energy Daily Report \n' + response4)
 
 """# gives 401 is no consumption meter
 response8 = requests.get(
@@ -83,11 +83,14 @@ print('\n Equipmet Inventory \n' + response4)"""
     'https://api.enphaseenergy.com/api/v2/systems/2527105/stats?datetime_format=iso8601',  params=params).text
 print('\n Equipment Stats \n' + response5)"""
 
-"""response6 = requests.get(
+response6 = requests.get(
     'https://api.enphaseenergy.com/api/v2/systems/inverters_summary_by_envoy_or_site?site_id=2527105',  params=params).text  # for loop for solar array
-print('\n Inverters \n' + response6)"""
+print('\n Inverters \n' + response6)
+for i in response6["micro_inverters"]:  # inverter_summary
+    inverters = str(["micro_inverters"])
+    print(inverters)    # inverter
 
-system_id = '2527105'
+"""system_id = '2527105'
 response = requests.get(
     'https://api.enphaseenergy.com/api/v2/systems/' + system_id + '/summary',  params=params)
 #print('\n Summary \n' + response)
@@ -104,7 +107,7 @@ print('\n System kWh Life Time\n', jsonResponse["energy_lifetime"]/1000)
 
 # print(response3["system_id"])
 # for key, value in response7:
-#    print(value)
+#    print(value)"""
 
 
 # NB. Original query string below. It seems impossible to parse and
